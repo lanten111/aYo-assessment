11f0ce3c790516e2616cee8ff567d1333e059b0c
Makhado <muhumbulo.makhado@momentum.co.za>
2022-02-10 00:26:32 +0200

    first commit

---
+++ ayo-assessment/src/main/java/co/za/ayoassessment/AyoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package co.za.ayoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyoAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyoAssessmentApplication.class, args);
+    }
+
+}
---
+++ ayo-assessment/src/test/java/co/za/ayoassessment/AyoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.ayoassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyoAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
