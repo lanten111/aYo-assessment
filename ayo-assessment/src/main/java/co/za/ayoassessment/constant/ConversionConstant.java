b77627491a5c807effcbd3aa4e94d2e0b7158274
Makhado <muhumbulo.makhado@momentum.co.za>
2022-02-10 20:43:41 +0200

    added constants

---
+++ ayo-assessment/src/main/java/co/za/ayoassessment/constant/ConversionConstant.java
@@ -0,0 +1,14 @@
+package co.za.ayoassessment.constant;
+
+public class ConversionConstant {
+
+    public static double FEET_MITER_CONSTANT =  3.280839895;
+    public static double CELCIUS_FAHRENHEIT_CONSTANT = 32;
+    public static double BAR_PSI_CONSTANT = 14.503773773;
+    public static double PSI_BAR_CONSTANT =  0.0689476;
+    public static double KILO_POUNDS_CONSTANT = 2.2046226218;
+    public static double POUNDS_KILO_CONSTANT = 0.45359237;
+    public static double GALLONS_LITERS_CONSTANT = 3.785411784;
+    public static double LITERS_GALLONS_CONSTANT = 0.2641720524;
+
+}
---
+++ ayo-assessment/src/main/java/co/za/ayoassessment/constant/Symbol.java
@@ -0,0 +1,15 @@
+package co.za.ayoassessment.constant;
+
+public enum Symbol {
+    kg,
+    lbs,
+    m,
+    in,
+    C,
+    F,
+    ft,
+    psi,
+    bar,
+    gal_US,
+    L
+}
